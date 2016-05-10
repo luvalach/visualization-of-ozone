87cce569e3b211b0c7e5704060a61612ed574d36
Lukas Valach <luvalach@seznam.cz>
2016-05-10 21:40:30 +0200

    Equals and HashCode methods added into Source entity.

--- src/main/java/cz/muni/fi/sdipr/visualizationofozone/model/Source.java
+++ src/main/java/cz/muni/fi/sdipr/visualizationofozone/model/Source.java
@@ -70,4 +70,47 @@ public class Source implements BaseEntity<Long> {
 	public void setTableHeaderPattern(String tableHeaderPattern) {
 		this.tableHeaderPattern = tableHeaderPattern;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((tableHeaderPattern == null) ? 0 : tableHeaderPattern.hashCode());
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Source other = (Source) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (tableHeaderPattern == null) {
+			if (other.tableHeaderPattern != null)
+				return false;
+		} else if (!tableHeaderPattern.equals(other.tableHeaderPattern))
+			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		return true;
+	}
 }
