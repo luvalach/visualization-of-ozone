6ff5d3a1076a30d7e82226ff528eec12b6aa9830
Lukas Valach <luvalach@seznam.cz>
2016-05-22 15:54:21 +0200

    Measurement converter replace negative values by zero.

--- src/main/java/cz/muni/fi/sdipr/visualizationofozone/converter/MeasurementConvertor.java
+++ src/main/java/cz/muni/fi/sdipr/visualizationofozone/converter/MeasurementConvertor.java
@@ -5,12 +5,21 @@ import java.util.List;
 
 import cz.muni.fi.sdipr.visualizationofozone.model.Measurement;
 
+/**
+ * Measurement converter converts list of measurements to form needed by
+ * ZingChart component. All values far below zero will be considered as
+ * measurement error and will be replaced by zero.
+ * 
+ * @author Lukas
+ *
+ */
 public class MeasurementConvertor {
 	public static List<Number[]> toChartReadableArray(List<Measurement> measurements) {
 		List<Number[]> measurementsArray = new ArrayList<Number[]>();
 
 		for (Measurement m : measurements) {
-			measurementsArray.add(new Number[] { m.getDateTime().getTime(), m.getValue() });
+			Float value = m.getValue() < -1000000000 ? null : m.getValue();
+			measurementsArray.add(new Number[] { m.getDateTime().getTime(), value });
 		}
 		return measurementsArray;
 	}
